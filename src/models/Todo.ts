276342d6d70822bdf3b56b0ca55ff8a7a02ffead
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2025-06-03 15:44:19 +0200

    feat: added class for Todo

--- src/models/Todo.ts
+++ src/models/Todo.ts
@@ -1,1 +1,16 @@
-// Skapa ritning för todo genom class och använd constructor 
+export class Todo {
+    id: number; 
+    title: string;
+    emotion: string;
+    isDone: boolean;
+
+    constructor (title: string, emotion: string, isDone: boolean) {
+
+        this.id = Date.now() + Math.random();
+        this.title = title;
+        this.emotion = emotion;
+        this.isDone = isDone;
+
+    }
+
+}
