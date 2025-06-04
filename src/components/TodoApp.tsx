9b62aa19a9ca83b0813446be48cf2530233e3a35
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2025-06-04 17:30:55 +0200

    feat: added filter by emotion

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -37,13 +37,22 @@ export const TodoApp = () => { // Bygger hela appen
         );
       };
 
-      const [sortNewTodoFirst, setSortNewTodoFirst] = useState(true);
+    const [sortNewTodoFirst, setSortNewTodoFirst] = useState(true);
+    const [sortNewInactiveTodoFirst, setSortNewInactiveTodoFirst] = useState(false);
+
+    const [emotionFilter, setEmotionFilter] = useState('all');
+    const [emotionDoneFilter, setEmotionDoneFilter] = useState('all');
 
       const sortedActiveTodos = [...todoList]
-      .filter(todo => !todo.isDone)
-      .sort((a,b) => {
-        return sortNewTodoFirst ? b.id - a.id : a.id - b.id;
-      });
+        .filter(todo => !todo.isDone) // bara ofärdiga
+        .filter(todo => emotionFilter === 'all' || todo.emotion === emotionFilter) // känsla
+        .sort((a, b) => sortNewTodoFirst ? b.id - a.id : a.id - b.id); // sortera
+
+        const sortedInactiveTodos = [...todoList]
+        .filter(todo => todo.isDone) 
+        .filter(todo => emotionDoneFilter === 'all' || todo.emotion === emotionDoneFilter) // känsla
+        .sort((a, b) => sortNewInactiveTodoFirst ? b.id - a.id : a.id - b.id); // sortera
+
 
       const getBgColorForEmotion = (todo: Todo) => {
         if (!todo.isDone) return 'bg-white';
@@ -83,24 +92,44 @@ export const TodoApp = () => { // Bygger hela appen
 
         <div className="grid grid-cols-1 mt-50 md:grid-cols-2 gap-20">
         <ul>
-            <h2 className="text-lg font-bold mb-2 uppercase">Todos</h2>
-            <div>
-                <button onClick={()=> setSortNewTodoFirst(prev => !prev)}>{sortNewTodoFirst? <CalendarArrowDown/>: <CalendarArrowUp />}</button> 
-                <button>Emotion</button>
+            <h2 className="text-lg font-bold mb-3 uppercase">Todos</h2>
+            <div className="flex justify-end items-right mb-5">
+                <button className="!bg-transparent" onClick={()=> setSortNewTodoFirst(prev => !prev)}>{sortNewTodoFirst? <CalendarArrowDown/>: <CalendarArrowUp />}</button> 
+                <select className="text-center" value={emotionFilter} onChange={(e) => setEmotionFilter(e.target.value)}>
+                        <option value={'all'}>All</option>
+                        <option value={'fun'}>😃 Fun</option>
+                        <option value={'stressful'}>😰 Stressful</option>
+                        <option value={'important'}>❗ Important</option>
+                        <option value={'boring'}> 😒 Boring</option>
+                </select>
                 </div>
                 {sortedActiveTodos.map(todo => (
             <li key={todo.id} className={` flex justify-between items-center rounded-lg p-2 mb-2 transition-colors duration-300 bg-yellow-50 ${getBgColorForEmotion(todo)}`}>
             <span>{getEmotionEmoji(todo.emotion)}<span className="ml-5">{todo.title}</span></span> 
             <div className="flex gap-2 ml-2">
-                <button onClick={() =>handleDeleteTodoByID(todo.id)}><Trash2 className="w-5 h-5"/></button>
+                
                 <button onClick={()=> toggleTodoIsDone(todo.id)}>{todo.isDone? <Undo2 />: <Check />}</button> 
+                <button onClick={() => {const confirmDelete = confirm("Are you sure you want to delete this todo?");
+                if (confirmDelete) {handleDeleteTodoByID(todo.id);}}}>
+                <Trash2 className="w-5 h-5" />
+                </button>
             </div>  
             </li>
             ))}
         </ul>
         <ul>
             <h2 className="text-lg font-bold mb-2 uppercase">Finished todos</h2>
-            {todoList.filter(todo=>todo.isDone).map(todo => (
+            <div className="flex justify-end items-right mb-5">
+                <button className="!bg-transparent" onClick={()=> setSortNewInactiveTodoFirst(prev => !prev)}>{sortNewInactiveTodoFirst? <CalendarArrowDown/>: <CalendarArrowUp />}</button> 
+                <select className="text-center" value={emotionDoneFilter} onChange={(e) => setEmotionDoneFilter(e.target.value)}>
+                        <option value={'all'}>All</option>
+                        <option value={'fun'}>😃 Fun</option>
+                        <option value={'stressful'}>😰 Stressful</option>
+                        <option value={'important'}>❗ Important</option>
+                        <option value={'boring'}> 😒 Boring</option>
+                </select>
+                </div>
+                {sortedInactiveTodos.map(todo => (
                 <li className="flex justify-between items-center rounded-lg p-2 mb-2 transition-colors duration-300 bg-yellow-50">
                 <span className="ml-2">{todo.title}</span> 
                 <div className="flex gap-2">
