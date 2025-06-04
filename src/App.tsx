e4e590c1a15b0bae7311d6e6d95b636404b1cf12
mikaelakihl <mikaela.kihl@medieinstitutet.se>
2025-06-04 15:19:18 +0200

    feat: added width for app mq

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TodoApp } from "./components/TodoApp";
 const App = () => {
 
   return (
-    <div className="min-h-screen bg-cover bg-center bg-yellow-100">
+    <div className=" md:w-[80vw] min-h-screen bg-cover bg-center bg-yellow-100">
       <TodoApp/>
     </div>
   )
